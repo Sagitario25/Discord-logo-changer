194ff910dbbe783b25177231b72e63edad563a5f
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-08-09 02:05:35 +0200

    Functional command line

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ com = command.Interpreter ()
 com.addCommand("getNames", gestor.getNames)
 com.addCommand("restore", gestor.callRestore)
 com.addCommand("change", gestor.callChange)
+
+while True:
+	com.call (input ("--- "))
