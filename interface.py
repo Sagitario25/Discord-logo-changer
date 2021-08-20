fddb519b6c90d3df3ad12756677c6a3065e26a2d
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-08-21 00:51:49 +0200

    Canvas constructor

--- interface.py
+++ interface.py
@@ -1,4 +1,23 @@
 import tkinter
 
+def constructCanvas (canvas, contents):
+	for i in contents:
+		print (i ["text"])
+		if   i ["type"] == "Label":
+			tkinter.Label (canvas, text = i["text"]).pack (fill = tkinter.BOTH, expand = True)
+		elif i ["type"] == "Button":
+			tkinter.Button (canvas, text = i ["text"], command = i ["command"]).pack (fill = tkinter.BOTH, expand = True)
+
 def mainMenu ():
-	pass
+	window = tkinter.Tk ()
+	canvas = tkinter.Canvas (window)
+
+	conts = [
+		{"type" : "Label", "text" : "Test text"},
+		{"type" : "Button", "text" : "Print \"a\"", "command" : lambda: print ("a")},
+		{"type" : "Button", "text" : "Clear", "command" : canvas.destroy}
+	]
+	constructCanvas(canvas, conts)
+
+	canvas.pack (fill = tkinter.BOTH, expand = True)
+	window.mainloop ()
