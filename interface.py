1ae8128efbfd24b84eddbe534543281f8b9cc811
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-08-21 13:29:03 +0200

    Reset canvas function

--- interface.py
+++ interface.py
@@ -1,5 +1,8 @@
 import tkinter
 
+window = tkinter.Tk ()
+canvas = tkinter.Canvas (window)
+
 def constructCanvas (canvas, contents):
 	for i in contents:
 		print (i ["text"])
@@ -8,14 +11,15 @@ def constructCanvas (canvas, contents):
 		elif i ["type"] == "Button":
 			tkinter.Button (canvas, text = i ["text"], command = i ["command"]).pack (fill = tkinter.BOTH, expand = True)
 
-def mainMenu ():
-	window = tkinter.Tk ()
+def resetCanvas (canvas):
+	canvas.destroy ()
 	canvas = tkinter.Canvas (window)
 
+def mainMenu ():
 	conts = [
 		{"type" : "Button", "text" : "Change Logo", "command" : canvas.destroy},
 		{"type" : "Button", "text" : "Restore default", "command" : canvas.destroy},
-		{"type" : "Button", "text" : "Convert images to icons", "command" : canvas.destroy},
+		{"type" : "Button", "text" : "Convert images to icons", "command" : lambda: resetCanvas (canvas)},
 		{"type" : "Button", "text" : "Help", "command" : canvas.destroy}
 	]
 	constructCanvas(canvas, conts)
