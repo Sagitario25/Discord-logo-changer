649549b704e8cc0c7e8dac446f97f698a1cc0c46
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-08-21 17:43:36 +0200

    Reestructured interface inner workings

--- interface.py
+++ interface.py
@@ -1,28 +1,34 @@
 import tkinter
 
-window = tkinter.Tk ()
-canvas = tkinter.Canvas (window)
+def newWindow ():
+	window = tkinter.Tk ()
+	return window
 
 def constructCanvas (canvas, contents):
 	for i in contents:
-		print (i ["text"])
 		if   i ["type"] == "Label":
 			tkinter.Label (canvas, text = i["text"]).pack (fill = tkinter.BOTH, expand = True)
 		elif i ["type"] == "Button":
 			tkinter.Button (canvas, text = i ["text"], command = i ["command"]).pack (fill = tkinter.BOTH, expand = True)
 
-def resetCanvas (canvas):
-	canvas.destroy ()
-	canvas = tkinter.Canvas (window)
-
 def mainMenu ():
-	conts = [
-		{"type" : "Button", "text" : "Change Logo", "command" : canvas.destroy},
-		{"type" : "Button", "text" : "Restore default", "command" : canvas.destroy},
-		{"type" : "Button", "text" : "Convert images to icons", "command" : lambda: resetCanvas (canvas)},
-		{"type" : "Button", "text" : "Help", "command" : canvas.destroy}
-	]
-	constructCanvas(canvas, conts)
+	window = newWindow ()
 
-	canvas.pack (fill = tkinter.BOTH, expand = True)
+	conts = [
+		{"type" : "Button", "text" : "Change Logo", "command" : lambda: chooseIcon (window)},
+		{"type" : "Button", "text" : "Restore default", "command" : window.destroy},
+		{"type" : "Button", "text" : "Convert images to icons", "command" : lambda: resetCanvas (canvas)},
+		{"type" : "Button", "text" : "Help", "command" : window.destroy}
+	]
+	constructCanvas (window, conts)
+	
 	window.mainloop ()
+
+def chooseIcon (lastWindow):
+	lastWindow.destroy ()
+	window = newWindow()
+
+	conts = [
+		{"type" : "Label", "text" : "Choose your icon"}
+	]
+	constructCanvas (window, conts)
