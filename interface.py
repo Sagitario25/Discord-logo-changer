361c81d5e40fa559e6fc5b57f4951994ccd3ff12
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-08-28 01:34:48 +0200

    More parameters for constructCanvas

--- interface.py
+++ interface.py
@@ -9,16 +9,29 @@ def newWindow ():
 	window.minsize (200, 0)
 	return window
 
-def constructCanvas (canvas, contents, side = tkinter.TOP):
+def constructCanvas (canvas, contents):
+	contents = defaultContents (contents)
 	for i in contents:
 		if   i ["type"] == "Label":
-			tkinter.Label (canvas, text = i["text"]).pack (fill = tkinter.BOTH, expand = True, side = side)
+			tkinter.Label (canvas, text = i["text"]).pack (fill = i ["fill"], expand = i ["expand"], side = i ["side"])
 		elif i ["type"] == "Button":
-			tkinter.Button (canvas, text = i ["text"], command = i ["command"]).pack (fill = tkinter.BOTH, expand = True, side = side)
+			tkinter.Button (canvas, text = i ["text"], command = i ["command"]).pack (fill = i ["fill"], expand = i ["expand"], side = i ["side"])
 		elif i ["type"] == "Canvas":
 			newCanvas = tkinter.Canvas (canvas)
-			constructCanvas (newCanvas, i ["contents"], side = tkinter.LEFT)
+			constructCanvas (newCanvas, i ["contents"])
 			newCanvas.pack (fill = tkinter.BOTH, expand = True)
+
+def defaultContents (conts):
+	for i in conts:
+		keys = [j for j in i]
+		if not "fill" in keys:
+			i ["fill"] = tkinter.BOTH
+		if not "expand" in keys:
+			i ["expand"] = True
+		if not "side" in keys:
+			i ["side"] = tkinter.TOP
+
+	return conts
 
 def mainMenu (lastWindow = None):
 	if lastWindow != None:
@@ -44,8 +57,8 @@ def chooseIcon (lastWindow):
 	]
 	for i in gestor.getNames ():
 		conts.append ({"type" : "Canvas", "contents" : [
-			{"type" : "Button", "text" : i, "command" : functools.partial (changeIcon, i)},
-			{"type" : "Button", "text" : "Preview", "command" : functools.partial (subprocess.run, [os.path.join (os.getenv ("windir"), "System32", "mspaint.exe"), os.path.join ("Icons", i + ".ico")])}
+			{"type" : "Button", "text" : i, "side" : tkinter.LEFT, "expand" : True, "command" : functools.partial (changeIcon, i)},
+			{"type" : "Button", "text" : "Preview", "side" : tkinter.LEFT, "expand" : False, "command" : functools.partial (subprocess.run, [os.path.join (os.getenv ("windir"), "System32", "mspaint.exe"), os.path.join ("Icons", i + ".ico")])}
 			]})
 	conts.append ({"type" : "Label", "text" : ""})
 	conts.append ({"type" : "Button", "text" : "Back to main menu", "command" : lambda: mainMenu (window)})
