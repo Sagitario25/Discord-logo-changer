fd2c33eb5bf9a55b39fe82a2731d5eb1c08f9891
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-09-08 01:09:07 +0200

    Integrated icon converter to commands

--- main.py
+++ main.py
@@ -30,10 +30,11 @@ if installed:
 def commandline ():
 	com = command.Interpreter ()
 
-	com.addCommand("getNames", gestor.getNames, "Prints all the names of available icons\nPlace icons in .ico format in \"Icons\" folder to add more\nI recomend the next page for converting images to icons\nhttps://image.online-convert.com/convert-to-ico")
+	com.addCommand("getNames", gestor.getNames, "Prints all the names of available icons\nPlace icons in .ico format in \"Icons\" folder to add more\nI recomend using the integrated icon converter (image2ico) to make the icons")
 	com.addCommand("restore", gestor.callRestore, "Restores the icon back to the original\nThe original is the icon that was in use when this program was executed for first time")
 	com.addCommand("change", gestor.callChange, "Changes Discord logo for the specified one\nPlace the name of the icon separated by an space\nIf the name has spaces in it it must be writed between quotation marks \"\"\nTo get possible names use getNames")
 	com.addCommand("repair", gestor.callRepair, "Changing the icon or restoring may cause some errors on Discord\nThis will try to repair them, it is not very advanced\nReinstalling Discord is endorsed if this does not work")
+	com.addCommand("image2ico", gestor.callImage2ico, "Converts any image into an ico,\nthen the ico can be set to be the Discord icon.\nThe name of the icon is the name of the image")
 
 	print ('Type "listCommands" to see a list of all available commands')
 	print ('Type "help" before any command to get help')
