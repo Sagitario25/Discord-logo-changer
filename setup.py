de0848036ea7138a081dd247fb97d70c731f9a23
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-12-11 02:13:37 +0100

    Updated for new structure

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+#Modules
+import gestor
+
 #Packages
 import sys
 import shutil
 import os
 
-sys.argv.append ("py2exe")#Auto call py2exe
 
-if os.path.exists ("dist"):#Remove previous build
-	shutil.rmtree ("dist")
+if os.path.exists ("Discord Logo Changer"):#Remove previous build
+	shutil.rmtree ("Discord Logo Changer")
 
 os.system ('pyinstaller --noconfirm --onefile --windowed --icon "D:/Informatica/Python/Discord/app.ico" --name "Discord Logo Changer"  "D:/Informatica/Python/Discord/main.py"')
 
@@ -15,4 +17,7 @@ os.remove ("Discord Logo Changer.spec")
 
 shutil.copy ("tool.exe", "dist/tool.exe")#Copy resource hacker
 shutil.copy ("app.ico", "dist/app.ico")#Copy resource hacker
-shutil.copytree ("Icons", "dist/Icons")#Copy default icons
+shutil.copytree (gestor.iconsPath, os.path.join ("dist", gestor.iconsPath))#Copy default icons
+shutil.copytree (gestor.fontsPath, os.path.join ("dist", gestor.fontsPath))#Copy default icons
+
+os.rename ("dist", "Discord Logo Changer")
